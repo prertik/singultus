09edf6b827d3a7d8c2d71a2b16964998b48b9581
Pratik Karki <predatoramigo@gmail.com>
2020-10-21 21:25:11 +0545

    Add url to deploy to clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject singultus "1.0.0"
+(defproject org.clojars.prertik/singultus "1.0.0"
   :description "A fork of crate (with modern changes) which is ClojureScript implementation of Hiccup"
+  :url "https://github.com/prertik/singultus"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.773"]])
