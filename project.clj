63de28a0d442d80803e0597f07cf1fd9d9cc9ad9
Pratik Karki <predatoramigo@gmail.com>
2020-10-21 20:45:43 +0545

    Deps of clojure and clojurescript

--- project.clj
+++ project.clj
@@ -1,2 +1,4 @@
 (defproject singultus "1.0.0"
-  :description "A fork of crate (with modern changes) which is ClojureScript implementation of Hiccup")
+  :description "A fork of crate (with modern changes) which is ClojureScript implementation of Hiccup"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.773"]])
